5f8e1edb84ceffefc4e77b23c9d3cd92f5a1ae2c
jEUGENEdev <genyamateyuk@gmail.com>
2024-03-28 02:20:49 +0300

    time spent as str

--- main.py
+++ main.py
@@ -6,13 +6,7 @@ class Test:
     def __init__(self, full_name: str, mark: float, time_spent: str):
         self.full_name = full_name
         self.mark = mark
-
-        try:
-            matches = findall(r'\d+', time_spent)
-            time_spent = (int(matches[0]), int(matches[1]))
-            self.time_spent = time_spent
-        except:
-            self.time_spent = ()
+        self.time_spent = time_spent
 
     def __str__(self):
         return f'{self.full_name}; {self.mark}; {self.time_spent}'
